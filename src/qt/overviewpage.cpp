e77270c79aa1951ddc6f4b8f81785da60bb8bd7a
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-12 19:31:02 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -680,12 +680,12 @@ void OverviewPage::DisablePrivateSendCompletely() {
 
 /*** HTH Logo Buttons ****/
 
-void QPushButton::on_pushButton_clicked()
+void QPushButton::on_pushButton_Website_clicked()
 {
   QDesktopServices::openUrl(QUrl("http://helpthehomelessworldwide.org", QUrl::TolerantMode));
 }
 
-void QPushButton::on_pushButton_clicked()
+void QPushButton::on_pushButton_Website_1_clicked()
 {
   QDesktopServices::openUrl(QUrl("http://hth.world", QUrl::TolerantMode));
 }
