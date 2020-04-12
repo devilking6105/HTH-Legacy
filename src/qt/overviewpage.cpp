2efc0ffaa6ad3cbb15bd67b31307a8f69f4f14b1
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-12 18:44:15 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -680,6 +680,9 @@ void OverviewPage::DisablePrivateSendCompletely() {
 
 /*** HTH Logo Buttons ****/
 
+QPushButton button = new QPushButton_Website();
+
+QPushButton button = new QPushButton_Website_1();
 
 void QPushButton::on_pushButton_Website_clicked()
 {
