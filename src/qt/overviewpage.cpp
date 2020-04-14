f3cbf7c2edb2dff3e475c3a6a45d74cc3af2e8f6
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-14 00:28:56 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -337,6 +337,39 @@ void OverviewPage::updateAlerts(const QString& warnings)
   //  this->ui->labelAlerts->setText(warnings);
 }
 
+
+                /**** Blockchain Information *****/
+
+void OverviewPage::updateBlockChainInfo()
+{
+    if (masternodeSync.IsBlockchainSynced())
+    {
+        int CurrentBlock = clientModel->getNumBlocks();
+        int64_t netHashRate = chainActive.GetNetworkHashPS(24, CurrentBlock-1);
+        double BlockReward = GetBlockValue(CurrentBlock);
+        double BlockRewardesbcoin =  static_cast<double>(BlockReward/COIN);
+        double CurrentDiff = GetDifficulty();
+
+        ui->label_CurrentBlock_value->setText(QString::number(CurrentBlock));
+
+        ui->label_Nethash->setText(tr("Difficulty:"));
+        ui->label_Nethash_value->setText(QString::number(CurrentDiff,'f',4));
+
+        ui->label_CurrentBlockReward_value->setText(QString::number(BlockRewardesbcoin, 'f', 1).append(" | ") + QString::number(GetSporkValue(SPORK_11_DEV_FEE)).append("%"));
+
+        ui->label_Supply_value->setText(QString::number(chainActive.Tip()->nMoneySupply / COIN).append(" ESBC"));
+
+        ui->label_24hBlock_value->setText(QString::number(block24hCount));
+        ui->label_24hPoS_value->setText(QString::number(static_cast<double>(posMin)/COIN,'f',1).append(" | ") + QString::number(static_cast<double>(posMax)/COIN,'f',1));
+        ui->label_24hPoSMedian_value->setText(QString::number(static_cast<double>(posMedian)/COIN,'f',1));
+    }
+}
+
+
+
+                /**** End Blockchain Information ******/
+
+
 void OverviewPage::showOutOfSyncWarning(bool fShow)
 {
     ui->labelWalletStatus->setVisible(fShow);
