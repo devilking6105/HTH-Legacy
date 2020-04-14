c1cd1480012a1a04aea8089a02f9d31acfff285e
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-13 22:41:25 -0400

    Update overviewpage.h

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -48,7 +48,6 @@ Q_SIGNALS:
 
 private:
     QTimer *timer;
-    QTimer* timerinfo_blockchain;
     Ui::OverviewPage *ui;
     ClientModel *clientModel;
     WalletModel *walletModel;
@@ -85,7 +84,6 @@ private Q_SLOTS:
     void on_pushButton_Website_2_clicked();
     void on_pushButton_Website_3_clicked();
     void on_pushButton_Website_4_clicked();
-    void updateBlockChainInfo();
 };
 
 #endif // BITCOIN_QT_OVERVIEWPAGE_H
